97c88ea8e59a468fa8f23080036d2c9381268c21
D054910 <j.buck@sap.com>
2016-12-01 14:45:38 +0100

    initial

---
+++ app/src/main/java/com/lofarolabs/mjpegandroid/mjpegAndroid.java
@@ -0,0 +1,73 @@
+package com.lofarolabs.mjpegandroid;
+
+/**
+ * Created by geovana on 3/26/15.
+ */
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class mjpegAndroid extends Activity {
+    private static final String TAG = "MjpegActivity";
+
+    private MjpegView mv;
+
+    public void onResume() {
+        super.onResume();
+
+        String URL = "http://192.168.1.83/camera/door?user=watcher&pwd=watcher";
+        mv = new MjpegView(this);
+        setContentView(mv);
+
+        new DoRead().execute(URL);
+    }
+
+    public void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "stop playback");
+        mv.stopPlayback();
+    }
+
+    public class DoRead extends AsyncTask<String, Void, MjpegInputStream> {
+        protected MjpegInputStream doInBackground(String... url) {
+            //TODO: if camera has authentication deal with it and don't just not work
+            HttpResponse res = null;
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            Log.d(TAG, "1. Sending http request");
+            try {
+                res = httpclient.execute(new HttpGet(URI.create(url[0])));
+                Log.d(TAG, "2. Request finished, status = " + res.getStatusLine().getStatusCode());
+                if(res.getStatusLine().getStatusCode()==401){
+                    //You must turn off camera User Access Control before this will work
+                    return null;
+                }
+                return new MjpegInputStream(res.getEntity().getContent());
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+                Log.d(TAG, "Request failed-ClientProtocolException", e);
+                //Error connecting to camera
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d(TAG, "Request failed-IOException", e);
+                //Error connecting to camera
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(MjpegInputStream result) {
+            mv.setSource(result);
+            mv.setDisplayMode(MjpegView.SIZE_BEST_FIT);
+            mv.showFps(true);
+        }
+    }
+}
